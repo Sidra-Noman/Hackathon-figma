905b5a0ddffc41414eb23ab35d1e7e9e6d31805e
Sidra Noman <sidranoman09@gmail.com>
2025-02-04 01:34:00 +0500

    push changes in this repo

--- app/components/Gearup.tsx
+++ app/components/Gearup.tsx
@@ -7,11 +7,32 @@ const Gearup = () => {
     {/* Best Of Air Max start */}
     <div className="px-4 sm:px-8 md:px-12 lg:px-20 pt-12">
       <div className="flex flex-col md:flex-row justify-between items-center md:items-start">
-        <h3 className="text-[18px] md:text-[22px] font-medium">
-          Best of Air Max
+        <h3 className="text-[18px] md:text-[23px] font-medium">
+        Gear Up
         </h3>
-        <div className="flex gap-2 mt-2 md:mt-0">
-          <p className="text-[14px] md:text-[15px] pt-1 md:pt-4 pr-2">Shop</p>
+
+        <div className="flex gap-2 mt-2 md:mt-0 pr-[58px]">
+          <p className="text-[14px] md:text-[15px] pt-1 md:pt-4 pr-2">Shop Men's</p>
+          <Image
+            src="/images/Frame (1).png"
+            alt="frame1"
+            width={32}
+            height={32}
+            className="w-8  h-8 md:w-12 md:h-12"
+          />
+          <Image
+            src="/images/Frame (2).png"
+            alt="frame2"
+            width={32}
+            height={32}
+                className="w-8  h-8 md:w-12 md:h-12"
+          />
+        </div>
+
+
+
+        <div className="flex gap-2 mt-2 md:mt-0 pr-[58px]">
+          <p className="text-[14px] md:text-[15px] pt-1 md:pt-4 pr-2">Shop Women's</p>
           <Image
             src="/images/Frame (1).png"
             alt="frame1"
@@ -37,7 +58,7 @@ const Gearup = () => {
         {/* Image 1 start */}
         <div className="w-full">
          <Image
-            src="/images/Image (1).png"
+            src="/images/Image (5).png"
             alt="image 1"
             width={441.36}
             height={441.36}
@@ -45,19 +66,19 @@ const Gearup = () => {
           />
           <div className="flex justify-between pt-4">
             <p className="text-[14px] md:text-[15px] font-medium">
-              Nike Air Max Pulse
+            Nike Dri-FIT ADV TechKnit Ultra
             </p>
-            <p className="text-[14px] md:text-[15px] font-medium">₹ 13 995</p>
+            <p className="text-[14px] md:text-[15px] font-medium">₹ 3 895</p>
           </div>
-          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575]">
-            Women&apos;s Shoes
+          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575] w-[180px]">
+          Men's Short-Sleeve Running Top
           </p>
         </div>
       
         {/* Image 2 start */}
         <div className="w-full ">
           <Image
-            src="/images/Image (2).png"
+            src="/images/Image (6).png"
             alt="image 2"
             width={441.36}
             height={441.36}
@@ -65,21 +86,21 @@ const Gearup = () => {
           />
           <div className="flex justify-between pt-4">
             <p className="text-[14px] md:text-[15px] font-medium">
-              Nike Air Max 97 SE
+            Nike Dri-FIT Challenger
             </p>
             <p className="text-[14px] md:text-[15px] font-medium">
-              ₹ ₹ 16 995
+            ₹ 2 495
             </p>
           </div>
-          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575]">
-            Men&apos;s Shoes
+          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575] w-[240px]">
+          Men's 18cm (approx.) 2-in-1 Versatile Shorts
           </p>
         </div>
         
         {/* Image 3 start */}
         <div className="w-full">
           <Image
-            src="/images/Image (3).png"
+            src="/images/Image (7).png"
             alt="image 1"
             width={441.36}
             height={441.36}
@@ -87,19 +108,19 @@ const Gearup = () => {
           />
           <div className="flex justify-between pt-4">
             <p className="text-[14px] md:text-[15px] font-medium">
-              Nike Air Max Pulse
+            Nike Dri-FIT ADV Run Division
             </p>
-            <p className="text-[14px] md:text-[15px] font-medium">₹ 13 995</p>
+            <p className="text-[14px] md:text-[15px] font-medium">₹ 5 295</p>
           </div>
-          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575]">
-            Men&apoa;s Shoes
+          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575] w-[200px]">
+          Women's Long-Sleeve Running Top
           </p>
 
         </div>
-         {/* Image 3 start */}
+         {/* Image 4 start */}
          <div className="w-full">
           <Image
-            src="/images/Image (3).png"
+            src="/images/Image (8).png"
             alt="image 1"
             width={441.36}
             height={441.36}
@@ -107,17 +128,18 @@ const Gearup = () => {
           />
           <div className="flex justify-between pt-4">
             <p className="text-[14px] md:text-[15px] font-medium">
-              Nike Air Max Pulse
+            Nike Fast
             </p>
-            <p className="text-[14px] md:text-[15px] font-medium">₹ 13 995</p>
+            <p className="text-[14px] md:text-[15px] font-medium">₹ 3 795</p>
           </div>
-          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575]">
-            Men&apoa;s Shoes
+          <p className="text-[14px] md:text-[15px] pt-2 text-[#757575] w-[220px]">
+          Women's Mid-Rise 7/8 Running Leggings with Pockets
           </p>
         </div>
         
       </div>
     </div>
+    
   </div>
 
 
